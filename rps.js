9ddf04f09929693527f0fbe5327b29666c457533
NURAH <Nurah.Muhammad@detroitLabs.com>
2017-03-23 16:03:21 -0400

    Adding an else if statement

--- rps.js
+++ rps.js
@@ -1,3 +1,25 @@
 var userChoice = prompt("Do you choose rock, paper, scissors, lizard, or spock?");
 var computerChoice = Math.random();
 var userChoice = prompt("Do you choose rock, paper,scissors,lizard, spock?");
+
+if (computerChoice < 0.2) {
+    computerChoice = "rock";
+} else if (computerChoice <= 0.4) {
+    computerChoice = "paper";
+} else if (computerChoice <= 0.6) {
+    computerChoice = "scissors";
+} else if (computerChoice <= 0.8) {
+    computerChoice = "lizard";
+} else {
+    computerChoice = "spock";
+}
+
+alert("The computer chose " + computerChoice);
+
+var compare = function(choice1, choice2){
+
+};
+    if (choice1 === choice2) {
+        alert("And... It's a tie!");
+    }
+    
